19dd4ba2824ad1d7eb618415bff1d1e31940b8de
Akash-2006 <freeshu966@gmail.com>
2025-06-17 11:02:09 +0530

    added welcome page and dashboard using routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { WelcomePage } from './welcome-page/welcome-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomePage,
+  },
+  {
+    path: 'Dashboard',
+    loadComponent: () =>
+      import('./dashboard-page/dashboard-page').then((m) => m.DashboardPage),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+interface RouteType {
+  name: string;
+  path: string;
+  isSelected: boolean;
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'routers';
+  private router = inject(Router);
+  routes: RouteType[] = [
+    { name: 'Home', path: '/', isSelected: true },
+    { name: 'Dashboard', path: '/Dashboard', isSelected: false },
+  ];
+  private periousOption: RouteType = this.routes[0];
+  select(routeName: string) {
+    this.periousOption.isSelected = false;
+    const selectedRoute = this.routes.find((r) => r.name === routeName);
+    if (selectedRoute) {
+      selectedRoute.isSelected = true;
+      this.periousOption = selectedRoute;
+    }
+  }
+  ngOnInit() {
+    const currentPath = this.router.url;
+    console.log(currentPath);
+    const match = this.routes.find((route) => route.path === currentPath);
+    if (match) {
+      this.select(match.name);
+    }
+  }
 }
